45649719d9c8826529dc8254deca6710278b3327
Lucas Moraes Matos <lucas11moraes@hotmail.com>
2025-10-14 17:23:11 -0300

    Initialize Nuxt app with Tailwind and API fetch
    Set up a new Nuxt project with Tailwind CSS integration. Added a basic app.vue that fetches character data from the Rick and Morty API and displays it. Included initial configuration, dependencies, and project documentation.

---
+++ app/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-blue-600">trojana</h1>
+
+    <div class="flex w-full max-w-[1224px] gap-4 mx-auto">
+      <div v-for="currentCharacter of data">
+        <p>{{ currentCharacter }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const { data, status, error, refresh, clear } = await useFetch('https://rickandmortyapi.com/api/character', {
+})
+
+console.log(data.value);
+</script>
